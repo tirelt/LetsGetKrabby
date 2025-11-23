d6564cb7fc1218e951d48efaf32c6cf7654831b4
Mac theo <theotirel@gmail.com>
2025-11-23 20:41:03 +0800

    finish enum

---
+++ enum_/src/main.rs
@@ -0,0 +1,15 @@
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main(){
+    let _home = IpAddr::V4(String::from("127.0.0.1"));
+    let _loopback = IpAddr::V6(String::from("::1"));
+}
