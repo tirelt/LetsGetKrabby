b1889c81db09800c738d5a2b3fd1eeb119a22d2c
theo <theotirel@gmail.com>
2025-11-28 22:30:21 +0800

    make it better

--- collections_2/src/main.rs
+++ collections_2/src/main.rs
@@ -6,24 +6,20 @@ fn main() {
     io::stdin()
         .read_line(&mut input)
         .unwrap();
-    input.pop();
-    let init_string = input.clone();
-    let mut ite =input.chars();  
-    let first = ite.next().unwrap();
-    let mut new_string = String::new();
-    let start_with_vowel = vowels.contains(&first);
-    if start_with_vowel{
-        new_string.push(first);
-    } 
-    for c in ite{
-        new_string.push(c);
+    let input =    input.trim();
+    let mut res = String::new();
+    for word in input.split_whitespace(){
+        let mut ite = word.chars();  
+        if let Some(first) = ite.next() {
+            if vowels.contains(&first){
+                let new_word = format!("{word}-hay ");
+                res.push_str(&new_word);
+            } else {
+                let rest : String = ite.collect();
+                let new_word = format!("{rest}-{first}ay ");
+                res.push_str(&new_word);
+            }
+        }
     }
-    if start_with_vowel{
-        new_string.push_str("-hay");
-    } else {
-        new_string.push('-');
-        new_string.push(first);
-        new_string.push_str("ay");
-    }
-    println!("{init_string} -> {new_string}");
+    println!("{res}");
 }
