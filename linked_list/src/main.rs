a685c6b90f470f92d8e8704419db4ce53f37adca
Mac theo <theotirel@gmail.com>
2025-11-29 17:09:45 +0800

    finished linked list

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,7 +1,8 @@
 struct LinkedList<'a,T> {
-    head: Option<T>,
-    tail: Option<&'a mut LinkedList<'a,T>>
+    head: Option<&'a T>,
+    tail: Option<Box<LinkedList<'a,T>>>
 }
+            
 impl<'a,T> LinkedList<'a,T>{
     fn new()->LinkedList<'a,T>{
         LinkedList{
@@ -9,7 +10,52 @@ impl<'a,T> LinkedList<'a,T>{
             tail: None,
         }
     }
+    fn push(&mut self,el: &'a T){
+        match self.head {
+            None => self.head = Some(el),
+            Some(_) => {
+                if self.tail.is_none() {
+                    self.tail = Some(Box::new(LinkedList::new())); 
+                }
+                self.tail.as_mut().unwrap().push(el);
+            }
+        }
+    }
+    fn len(&self)->u32{
+        match self.head {
+            None => 0,
+            Some(_) => 1 + match &self.tail {
+                None => 0,
+                Some(t) => t.len(),
+            }
+        }
+    }
+    fn pop(&mut self){
+        match self.head {
+            None => (),
+            Some(_) => {
+                if self.tail.is_none() {
+                    self.head = None;
+                } else {
+                    self.tail.as_mut().unwrap().pop();
+                }
+            }
+        }
+    }
 }
 fn main() {
-    let list: LinkedList<'_, String> = LinkedList::new();
+    let mut list: LinkedList<'_, String> = LinkedList::new();
+    let first = String::from("Theo");
+    let second = String::from("Tirel");
+    let third = String::from("le boss");
+    list.push(&first);
+    let mut size = list.len();
+    println!("{size}");
+    list.push(&second);
+    list.push(&third);
+    size = list.len();
+    println!("{size}");
+    list.pop();
+    size = list.len();
+    println!("{size}");
 }
